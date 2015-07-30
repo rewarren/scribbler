bfee4eaa52c5ebe26e4a36ae2121cd1d68bc3850
Rachel Warren <raelwa@gmail.com>
2015-07-30 17:39:50 -0400

    create/edit/delete posts, edit/delete comments working

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.all
+    @posts = Post.all.order(:id).reverse
   end
 
 
@@ -29,15 +29,22 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
+    @post.update(post_params)
+
+    redirect_to post_path(@post)
   end
 
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to posts_path
   end
 
   private
   def post_params
-    params.require(:post).permit(:author, :data, :title, :content)
+    params.require(:post).permit(:author, :date, :title, :content)
   end
 
 end
