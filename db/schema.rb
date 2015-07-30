f135fe30feda115cc9f98ac8526e37fb4159d6c9
Rachel Warren <raelwa@gmail.com>
2015-07-30 17:52:05 -0400

    generated Users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150729141059) do
+ActiveRecord::Schema.define(version: 20150730214415) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,11 @@ ActiveRecord::Schema.define(version: 20150729141059) do
     t.string "date"
     t.string "title"
     t.text   "content"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
   end
 
   add_foreign_key "comments", "posts"
